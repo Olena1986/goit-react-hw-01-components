7c9a709caf129dd500f0287366c68a5c3b7fbc74
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-05-15 19:48:35 +0200

    f

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import Profile from './profile'
+
+
+
+
+export const App = (props) => {
+  const user = {
+    username: 'Petra Marica',
+    tag: 'pmarica',
+    location: 'Salvador, Brasil',
+    avatar: 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
+    stats: {
+      followers: 1000,
+      views: 2000,
+      likes: 3000,
+    },
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
     </div>
   );
 };
